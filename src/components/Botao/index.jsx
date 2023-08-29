57745dfc6dc19a8bc62cd17b44f950a7e66ad01e
JooseJunior <josejuniorvha@gmail.com>
2023-08-28 21:23:17 -0300

    Upload componente botão

---
+++ src/components/Botao/index.jsx
@@ -0,0 +1,60 @@
+import styles from './styles.module.css';
+import { useState } from 'react';
+
+export default function Botao() {
+    const [valor, setValor] = useState();
+    const [mostrarMensagem, setMostrarMensagem] = useState(false);
+
+    function enviar(e) {
+        e.preventDefault();
+        setMostrarMensagem(true);
+        console.log("O conteúdo foi útil: " + valor);
+    }
+
+    function voltarParaBotoes() {
+        setMostrarMensagem(false);
+
+    }
+
+    return (
+        <header>
+            <form onSubmit={enviar} className={styles.fundo}>
+                {mostrarMensagem ? (
+                    <div className={styles.header}>
+                        <div className={styles.fund}>
+                            <div className={styles.textTitulo}>
+                                <h1>Obrigado!</h1>
+                            </div>
+                            <div className={styles.textMensagem}>
+                                <p>Sua opinião é muito importante para melhorarmos a qualidade dos nossos conteúdos!</p>
+                            </div>
+
+                        </div>
+                        <button className={styles.buttonVoltar} onClick={voltarParaBotoes}>
+                            Voltar
+                        </button>
+                    </div>
+                ) : (
+                    <div className={styles.bar}>
+                        <h1 className={styles.namebar}>Esse conteúdo foi útil?</h1>
+                        <div>
+                            <button
+                                className={styles.buttonbar1}
+                                onClick={() => setValor('Sim')}
+                                
+                            >
+                                😄Sim
+                            </button>
+                            <button
+                                className={styles.buttonbar2}
+                                onClick={() => setValor('Não')}
+                            >
+                                😢Não
+                            </button>
+                        </div>
+                    </div>
+                )}
+            </form>
+        </header>
+    );
+}
